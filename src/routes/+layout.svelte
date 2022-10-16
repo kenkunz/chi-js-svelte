cc0ad439b87d823409d62980f3ff120f64b19393
Ken Kunz <kennethkunz@gmail.com>
2022-10-16 11:28:59 -0500

    add postcss & preset-env

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<slot />
+
+<style lang="postcss" global>
+	:root {
+		font: 16px/20px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	}
+
+	input,
+	button {
+		font: inherit;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import postcssPresetEnv from 'postcss-preset-env';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+
+	preprocess: preprocess({
+		postcss: {
+			plugins: [postcssPresetEnv({ features: { 'nesting-rules': true } })]
+		}
+	})
 };
 
 export default config;
